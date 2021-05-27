5dfcbfc516864d49aec330b510a80a55390bfdd1
qif-equinor <qif@equinor.com>
2021-05-27 14:45:15 +0200

    add flask main app

---
+++ python/app_config.py
@@ -0,0 +1,9 @@
+import os
+
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+AUTHORITY = "https://login.microsoftonline.com/3aa4a235-b6e2-48d5-9195-7fcf05b459b0"  # For multi-tenant app
+REDIRECT_PATH = "/getAToken"
+GRAPH_ENDPOINT = 'https://graph.microsoft.com/v1.0/users'  # This resource requires no admin consent
+SCOPE = ["User.ReadBasic.All"]
+SESSION_TYPE = "filesystem"  # Specifies the token cache should be stored in server-side session
