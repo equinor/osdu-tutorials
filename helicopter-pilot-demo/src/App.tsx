3583c2fabed547bfc77c4230299bca3927887480
qif-equinor <qif@equinor.com>
2021-10-07 08:37:17 +0200

    add not found page

--- helicopter-pilot-demo/src/App.tsx
+++ helicopter-pilot-demo/src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {AzureAD} from "react-aad-msal";
+import {NotFoundPage} from "./pages/not-found/NotFoundPage";
 import './App.css';
+
+import {authProvider} from "./authProvider";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AzureAD provider={authProvider} forceLogin={true}>
+          <Router>
+              <Switch>
+                  <Route path="*" component={NotFoundPage}></Route>
+              </Switch>
+
+          </Router>
+      </AzureAD>
     </div>
   );
 }
---
+++ helicopter-pilot-demo/src/pages/not-found/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export function NotFoundPage() {
+    return (
+        <div className="not-found">
+            <h2>Sorry. The page you are looking for was not found...</h2>
+        </div>
+    )
+}
