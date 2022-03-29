d0d1c53a0c33538a177d2daf4bcfc36f0de3d96e
LeivHalvorL <101805377+LeivHalvorL@users.noreply.github.com>
2022-03-29 13:39:16 +0200

    tried to fix

--- react-well-demo/src/index.tsx
+++ react-well-demo/src/index.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import store from "./store";
-import {Provider} from "react-redux";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-    
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
