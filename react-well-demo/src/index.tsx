4642e9d19aa0385fdc461c284a268dfd64ed0aff
LeivHalvorL <101805377+LeivHalvorL@users.noreply.github.com>
2022-03-29 15:49:21 +0200

    import bootstrap.css

--- react-well-demo/src/index.tsx
+++ react-well-demo/src/index.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store";
 import { Provider } from "react-redux";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <Provider store={store}>
