aa8dee747ec2db5a017cd2f6249d46d91b62e6b7
Gorm Aarsheim <gormaarsheim@Gorms-MacBook-Pro.local>
2022-05-24 09:36:49 +0200

    Changed clientId in authProvider

--- react-well-demo/src/authProvider.ts
+++ react-well-demo/src/authProvider.ts
@@ -4,32 +4,32 @@ import { MsalAuthProvider, LoginType } from "react-aad-msal";
 // Avoid creating an instance inside the component it will be recreated on each render.
 // If two providers are created on the same page it will cause authentication errors.
 export const authProvider = new MsalAuthProvider(
-    {
-        auth: {
-            authority: "https://login.microsoftonline.com/3aa4a235-b6e2-48d5-9195-7fcf05b459b0",
-            clientId: "6a47658e-b1b7-45d0-adf0-50d81647c161",
-            postLogoutRedirectUri: window.location.origin,
-            redirectUri: window.location.origin,
-            validateAuthority: true,
+  {
+    auth: {
+      authority: "https://login.microsoftonline.com/",
+      clientId: "d91a40ba-e6ee-47ab-8836-cdd3c68b87d4",
+      postLogoutRedirectUri: window.location.origin,
+      redirectUri: window.location.origin,
+      validateAuthority: true,
 
-            // After being redirected to the "redirectUri" page, should user
-            // be redirected back to the Url where their login originated from?
-            navigateToLoginRequestUrl: false
-        },
+      // After being redirected to the "redirectUri" page, should user
+      // be redirected back to the Url where their login originated from?
+      navigateToLoginRequestUrl: false,
+    },
 
-        cache: {
-            cacheLocation: "sessionStorage",
-            storeAuthStateInCookie: false
-        }
+    cache: {
+      cacheLocation: "sessionStorage",
+      storeAuthStateInCookie: false,
     },
-    {
-        scopes: ["openid", "6a47658e-b1b7-45d0-adf0-50d81647c161/.default"]
-    },
-    {
-        loginType: LoginType.Popup,
-        // When a token is refreshed it will be done by loading a page in an iframe.
-        // Rather than reloading the same page, we can point to an empty html file which will prevent
-        // site resources from being loaded twice.
-        tokenRefreshUri: "http://localhost:3000"
-    }
+  },
+  {
+    scopes: ["openid", "d91a40ba-e6ee-47ab-8836-cdd3c68b87d4/.default"],
+  },
+  {
+    loginType: LoginType.Popup,
+    // When a token is refreshed it will be done by loading a page in an iframe.
+    // Rather than reloading the same page, we can point to an empty html file which will prevent
+    // site resources from being loaded twice.
+    tokenRefreshUri: "http://localhost:3000",
+  }
 );
